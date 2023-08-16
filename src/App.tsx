49e0c907c60fb41f57622641b3790713b0e6deb6
andrenormanlang <andrenormanlang@gmail.com>
2023-08-16 09:41:49 +0200

    excluded browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import Container from 'react-bootstrap/Container'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import Navigation from './pages/partials/Navigation'
-import HomePage from './pages/HomePage'
-import PageNotFound from './pages/PageNotFound'
-import RandomCatPage from './pages/RandomCatPage'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Container from 'react-bootstrap/Container';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './pages/partials/Navigation';
+import HomePage from './pages/HomePage';
+import PageNotFound from './pages/PageNotFound';
+import RandomCatPage from './pages/RandomCatPage';
+import CatBreed from './pages/CatBreed';
 
-import './assets/scss/App.scss'
-import CatBreed from './pages/CatBreed'
+import './assets/scss/App.scss';
 
 const App = () => {
-	return (
-		<div id="App">
-			<Navigation />
+  return (
+    <div id="App">
+      <Navigation />
 
-			<Container className="py-3">
-				<BrowserRouter basename="">
-					<Routes>
-						<Route path="/" element={<HomePage />} />
-						<Route path="/random-cat" element={<RandomCatPage />} />
-						<Route path="/random-cat-breed" element={<CatBreed />} />
+      <Container className="py-3">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/random-cat" element={<RandomCatPage />} />
+          <Route path="/random-cat-breed" element={<CatBreed />} />
 
-						<Route path="*" element={<PageNotFound />} />
-					</Routes>
-				</BrowserRouter>
-			</Container>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Container>
 
-			<ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-		</div>
-	)
-}
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </div>
+  );
+};
 
-export default App
-
+export default App;
