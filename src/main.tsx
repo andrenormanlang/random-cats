386ae81aa2be85af6b25dde7e18ab0c320052a92
andrenormanlang <andrenormanlang@gmail.com>
2023-09-02 00:07:25 +0200

    added hashrouter for vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter} from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
 
 
@@ -16,9 +16,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</QueryClientProvider>
 	</React.StrictMode>,
 )
